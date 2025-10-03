44be024fb4d0fe308cd337037c6cb2cf75ca9915
Ceyhan <ceyhanyildizt3@gmail.com>
2025-10-03 21:42:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'MTI5MDQwODM4ODU5Njg2MzA3MA.GJx32-.hCtjftsITuHfwaF4mMw0J0hALDqkjS4GEb7ObI', // Replace with your actual bot token
-    clientId: '1290408388596863070',  // Replace with your bot's client/application ID
-    guildId: '1081645825156984932'             // Replace with your server (guild) ID
+    token: 'DISCORD BOT TOKEN', // Replace with your actual bot token
+    clientId: 'BOT APPLICATION ID',  // Replace with your bot's client/application ID
+    guildId: 'SERVER ID'             // Replace with your server (guild) ID
 };
